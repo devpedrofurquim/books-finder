b2e512a695374faceb97f30e9d2eabf35bcc5c18
devpedrofurquim <pedrofurquim.dev@gmail.com>
2024-06-14 11:30:05 -0300

    feat: Add link to Amazon on Book Unity page

--- src/types/book.ts
+++ src/types/book.ts
@@ -4,5 +4,8 @@ export type Book = {
     key: string;
     title: string;
     author_name: [string];
-    first_publish_year: number;   
+    first_publish_year: number;
+    first_sentence: string;
+    number_of_pages_median: number;
+    id_amazon: string[];
 }
--- src/views/app/book/index.tsx
+++ src/views/app/book/index.tsx
@@ -1,4 +1,4 @@
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, Linking, TouchableOpacity } from 'react-native';
 import React from 'react';
 import { RouteProp } from '@react-navigation/native';
 import { AppRoutes } from '../../../navigation/_types/navigation';
@@ -8,12 +8,33 @@ type BookUnityProps = {
 };
 
 const BookUnity: React.FC<BookUnityProps> = ({ route }) => {
+
   const { book } = route.params;
+
+  const openAmazonLink = (id: string) => {
+    const url = `https://www.amazon.com/dp/${id}`;
+    Linking.openURL(url);
+  };
+
+  const renderAmazonLink = (id: string) => {
+    return `https://www.amazon.com/dp/${id}`;
+  };
+
 
   return (
     <View>
       <Text>{book.author_name}</Text>
       <Text>{book.title}</Text>
+      <Text>{book.first_publish_year}</Text>
+      <Text>{book.first_sentence}</Text>
+      <Text>{book.number_of_pages_median}</Text>
+      {
+        book.id_amazon && (
+          <TouchableOpacity onPress={() => openAmazonLink(book.id_amazon[0])}>
+          <Text style={{ color: 'blue', textDecorationLine: 'underline' }}>{renderAmazonLink(book.id_amazon[0])}</Text>
+        </TouchableOpacity>
+        )
+      }
       <Image source={{uri: `https://covers.openlibrary.org/b/id/${book.cover_i | book.cover_id}-M.jpg`}} width={300} height={400}/>
     </View>
   );
